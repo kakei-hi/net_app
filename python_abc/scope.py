157d9c7a5b440c59334a791da4df07f4f94c3db7
KAKEI Hidekazu <61128445+kakei-hi@users.noreply.github.com>
2026-05-09 11:04:25 +0900

    scope.py改訂 globalキーワードの追加

--- python_abc/scope.py
+++ python_abc/scope.py
@@ -1,6 +1,15 @@
 x = 5
-def func():
+def func1():
     x = 10
     print(f"関数の中:{x}")
-func() 
+
+func1() 
+print(f"関数の外:{x}")
+
+def func2():
+    global x
+    x = 20
+    print(f"関数の中:{x}")
+
+func2()
 print(f"関数の外:{x}")
